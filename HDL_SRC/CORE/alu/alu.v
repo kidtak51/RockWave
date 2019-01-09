44670150b084b4b19257328ecc6bedfd2d11b890
plusOne <ta.shono@gmail.com>
2019-01-10 00:00:39 +0900

    alu追記。算術演算はsignedを使用して書いた。

--- HDL_SRC/CORE/alu/alu.v
+++ HDL_SRC/CORE/alu/alu.v
@@ -5,7 +5,7 @@
  * File Created: 2018/12/19 23:48
  * Author: Takuya Shono ( ta.shono+1@gmail.com )
  * *****
- * Last Modified: 2018/12/28 14:34
+ * Last Modified: 2019/01/09 23:55
  * Modified By: Takuya Shono ( ta.shono+1@gmail.com )
  * *****
  * Copyright 2018 - 2018  Project RockWave
@@ -30,8 +30,8 @@ module alu (
     
     parameter XLEN = 32;
 
-    input  wire [XLEN-1:0] aluin1;
-    input  wire [XLEN-1:0] aluin2;
+    input  wire signed [XLEN-1:0] aluin1;
+    input  wire signed [XLEN-1:0] aluin2;
     input  wire [2:0] funct3;
     input  wire funct7;
     output wire [XLEN-1:0] aluout;
@@ -41,21 +41,19 @@ module alu (
     
     function [XLEN-1:0] calc( 
         input [3:0] funct73,
-        input [XLEN-1:0] aluin1,
-        input [XLEN-1:0] aluin2
+        input signed [XLEN-1:0] aluin1,
+        input signed [XLEN-1:0] aluin2
         );
         begin
             case( funct73 )
                    4'b0000  : calc = aluin1 + aluin2; //ADD 加算
-                   4'b1000  : calc = aluin1 - aluin2; //SUB 減算//2の補数//要修正
-                   4'b0001  : calc = aluin1 << aluin2; //SLL 左シフト//下位5bitマスク//要修正
-                   4'b0010  : calc = aluin1 + aluin2; //SLT 符号付きで大小比較＆結果をレジスタにロード//要修正
-                   4'b0011  : calc = aluin1 + aluin2; //SLTU 符号なしで大小比較＆結果をレジスタにロード//要修正
-                   4'b0100  : calc = aluin1 ^ aluin2; //排他的論理和
-                   4'b0101  : calc = aluin1 + aluin2; //右論理シフト//要修正
-                   4'b1101  : calc = aluin1 + aluin2; //左論理シフト//要修正
+                   4'b1000  : calc = aluin1 - aluin2; //SUB 減算
+                   4'b0001  : calc = aluin1 << 5; //SLL 左シフト
+                   4'b0100  : calc = aluin1 ^ aluin2; //XOR 排他的論理和
+                   4'b0101  : calc = aluin1 >> 5; //SRL 右シフト
+                   4'b1101  : calc = aluin1 >>> 5; //SRA 右算術シフト
                    4'b0110  : calc = aluin1 | aluin2; //OR 論理和                   
-                   4'b0111  : calc = aluin1 & aluin2; //論理積
+                   4'b0111  : calc = aluin1 & aluin2; //AND 論理積
                    default : calc = 32'hXXXX_XXXX;
             endcase
         end
