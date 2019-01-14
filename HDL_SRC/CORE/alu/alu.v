e9e5983a3bf322540e718e6e1803d7a36a9cd717
plusOne <ta.shono@gmail.com>
2019-01-14 15:21:36 +0900

    commit漏れがあったため、再commitした。

--- HDL_SRC/CORE/alu/alu.v
+++ HDL_SRC/CORE/alu/alu.v
@@ -5,7 +5,7 @@
  * File Created: 2018/12/19 23:48
  * Author: Takuya Shono ( ta.shono+1@gmail.com )
  * *****
- * Last Modified: 2019/01/14 24:00
+ * Last Modified: 2019/01/14 14:51
  * Modified By: Takuya Shono ( ta.shono+1@gmail.com )
  * *****
  * Copyright 2018 - 2018  Project RockWave
@@ -29,30 +29,28 @@ module alu (
     
     parameter XLEN = 32;
 
-    input  wire signed [XLEN-1:0] aluin1; //alu入力信号1
-    input  wire signed [XLEN-1:0] aluin2; //alu入力信号2
+    input  wire [XLEN-1:0] aluin1; //alu入力信号1
+    input  wire [XLEN-1:0] aluin2; //alu入力信号2
     input  wire [3:0] funct_alu; //alu演算器選択信号
     output wire [XLEN-1:0] aluout; //alu演算結果信号
 
     function [XLEN-1:0] calc( 
-        input signed [XLEN-1:0] aluin1,
-        input signed [XLEN-1:0] aluin2,
-        input funct_alu
+        input [XLEN-1:0] aluin1,
+        input [XLEN-1:0] aluin2,
+        input [3:0] funct_alu
         );
 
         begin
-            case( funct_alu )
+            casex( funct_alu )
                    4'b0000  : calc = aluin1 + aluin2; //ADD//ADDI 加算
                    4'b1000  : calc = aluin1 - aluin2; //SUB 減算
-                   4'b0001  : calc = aluin1 << aluin2 [4:0]; //SLL//SLLI 左シフト
-                   4'bX010  : calc = (aluin1 < aluin2)? 1:0; //SLT //SLTI
-                   4'bX011  : calc = ($unsigned(aluin1) < $unsigned(aluin2))? 1:0; //SLTU //SLTIU
-                   4'bX100  : calc = aluin1 ^ aluin2; //XOR//XORI 排他的論理和
+                   4'bx001  : calc = aluin1 << aluin2[4:0]; //SLL//SLLI 左シフト
+                   4'bx100  : calc = aluin1 ^ aluin2; //XOR 排他的論理和
                    4'b0101  : calc = aluin1 >> aluin2 [4:0]; //SRL//SRLI 右シフト
-                   4'b1101  : calc = aluin1 >>> aluin2 [4:0]; //SRA//SRAI 右算術シフト
-                   4'bX110  : calc = aluin1 | aluin2; //OR//ORI 論理和                   
-                   4'bX111  : calc = aluin1 & aluin2; //AND//ANDI 論理積
-                   default : calc = 32'hXXXX_XXXX;
+                   4'b1101  : calc = $signed(aluin1) >>> $signed(aluin2 [4:0]); //SRA//SRAI 右算術シフト
+                   4'bx110  : calc = aluin1 | aluin2; //OR 論理和                   
+                   4'bx111  : calc = aluin1 & aluin2; //AND 論理積
+                   default : calc = 32'hxxxx_xxxx;
             endcase
         end
     endfunction
