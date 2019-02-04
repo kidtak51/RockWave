2955f2c4576535d1a6a987d4d475c312faf870aa
kidtak51 <45393331+kidtak51@users.noreply.github.com>
2019-02-05 07:09:44 +0900

    gtk waveの波形表示を容易にするためにwire宣言でpcとx1~x32に名前を付けた。

--- HDL_SIM/fw/core_tb.v
+++ HDL_SIM/fw/core_tb.v
@@ -5,7 +5,7 @@
  * File Created: 2019/01/25 07:14
  * Author: kidtak51 ( 45393331+kidtak51@users.noreply.github.com )
  * *****
- * Last Modified: 2019/02/02 07:11
+ * Last Modified: 2019/02/05 07:02
  * Modified By: kidtak51 ( 45393331+kidtak51@users.noreply.github.com )
  * *****
  * Copyright 2018 - 2019  Project RockWave
@@ -37,17 +37,48 @@ reg rst_n;
 reg clk;
 integer i;//forループ用
 
-// Clock
+
+//gtk wave 波形表示用
+wire [XLEN-1:0] pc = u_top_core.u_top_fetch.program_counter;
+wire [XLEN-1:0] x01_ra = u_top_core.u_register_file.x1out;
+wire [XLEN-1:0] x02_sp = u_top_core.u_register_file.x2out;
+wire [XLEN-1:0] x03_gp = u_top_core.u_register_file.x3out;
+wire [XLEN-1:0] x04_tp = u_top_core.u_register_file.x4out;
+wire [XLEN-1:0] x05_t0 = u_top_core.u_register_file.x5out;
+wire [XLEN-1:0] x06_t1 = u_top_core.u_register_file.x6out;
+wire [XLEN-1:0] x07_t2 = u_top_core.u_register_file.x7out;
+wire [XLEN-1:0] x08_s0_fp = u_top_core.u_register_file.x8out;
+wire [XLEN-1:0] x09_s1 = u_top_core.u_register_file.x9out;
+wire [XLEN-1:0] x10_a0 = u_top_core.u_register_file.x10out;
+wire [XLEN-1:0] x11_a1 = u_top_core.u_register_file.x11out;
+wire [XLEN-1:0] x12_a2 = u_top_core.u_register_file.x12out;
+wire [XLEN-1:0] x13_a3 = u_top_core.u_register_file.x13out;
+wire [XLEN-1:0] x14_a4 = u_top_core.u_register_file.x14out;
+wire [XLEN-1:0] x15_a5 = u_top_core.u_register_file.x15out;
+wire [XLEN-1:0] x16_a6 = u_top_core.u_register_file.x16out;
+wire [XLEN-1:0] x17_a7 = u_top_core.u_register_file.x17out;
+wire [XLEN-1:0] x18_s2 = u_top_core.u_register_file.x18out;
+wire [XLEN-1:0] x19_s3 = u_top_core.u_register_file.x19out;
+wire [XLEN-1:0] x20_s4 = u_top_core.u_register_file.x20out;
+wire [XLEN-1:0] x21_s5 = u_top_core.u_register_file.x21out;
+wire [XLEN-1:0] x22_s6 = u_top_core.u_register_file.x22out;
+wire [XLEN-1:0] x23_s7 = u_top_core.u_register_file.x23out;
+wire [XLEN-1:0] x24_s8 = u_top_core.u_register_file.x24out;
+wire [XLEN-1:0] x25_s9 = u_top_core.u_register_file.x25out;
+wire [XLEN-1:0] x26_s10 = u_top_core.u_register_file.x26out;
+wire [XLEN-1:0] x27_s11 = u_top_core.u_register_file.x27out;
+wire [XLEN-1:0] x28_t3 = u_top_core.u_register_file.x28out;
+wire [XLEN-1:0] x29_t4 = u_top_core.u_register_file.x29out;
+wire [XLEN-1:0] x30_t5 = u_top_core.u_register_file.x30out;
+wire [XLEN-1:0] x31_t6 = u_top_core.u_register_file.x31out;
+
+//clock
 initial
     clk = 0;
 always begin
     #5 clk = ~clk;
 end
 
-initial begin
-    
-
-end
 reg[AWIDTH-1:0] memDataStartAddr = 'h800;
 initial begin
     //initial
@@ -92,8 +123,8 @@ localparam ECALL = 32'h73;
 
 always @(posedge clk) begin
     if(u_top_core.u_instruction_decode.inst == ECALL) begin
-        $display("%s, %d",{"result, ", `INST_ROM_FILE_NAME}, u_top_core.u_register_file.x3out);
-        //$exit(u_top_core.u_register_file.x3out);
+        $display("%s, %d",{"result, ", `INST_ROM_FILE_NAME}, u_top_core.u_top_core.u_register_file.x3out);
+        //$exit(u_top_core.u_top_core.u_register_file.x3out);
         $finish;
 
     end
